9e1271e8751815deb7dd18d5490ed52cd662a526
bartlebee <bartlebee@a.fake>
2013-01-20 19:04:37 -0700

    initial commit

---
+++ piplex/piplex.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    pip less lexer
+    ~~~~~~~~~~~
+
+    Pygments lexer for less piped pip output.
+
+    :license: GNU I guess
+"""
+
+from pygments.lexer import RegexLexer, bygroups
+from pygments.token import *
+
+
+class PipLexer(RegexLexer):
+    name = 'Pip'
+    aliases = ['pip']
+    filenames = ['']
+
+    tokens = {
+        'root': [
+            (r' .*\n', Text),
+            (r'(^.*?)([\s-]{2})', bygroups(Package, Text))
+        ]
+    }
